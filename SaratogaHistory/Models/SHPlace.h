124a088d66e52ca2b4d1254442e7ef08106b6b74
Kabir Manghnani <kaju99@gmail.com>
2015-06-21 10:28:51 -0700

    Finished Models and storyboard layout. Woo, progress!

---
+++ SaratogaHistory/Models/SHPlace.h
@@ -0,0 +1,30 @@
+//
+//  SHPlace.h
+//  Saratoga History
+//
+//  Created by Kabir Manghnani on 06/21/15.
+//  Copyright (c) 2015 Kabir Manghnani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <JPSThumbnailAnnotation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface SHPlace : NSObject
+
+@property (nonatomic, assign) int index;
+@property (nonatomic, strong) NSString *placeTitle;
+@property (nonatomic, assign) float lat;
+@property (nonatomic, assign) float lng;
+@property (nonatomic, strong) NSString *address;
+@property (nonatomic, strong) NSString *descriptionText;
+@property (nonatomic, strong) NSArray *images;
+@property (nonatomic, strong) NSArray *imageCaptions;
+@property (nonatomic, strong) JPSThumbnail *annotationThumbnail;
+@property (nonatomic, strong) AVURLAsset *audioURLAsset;
+
+-(id)initWithIndex:(int)indx title:(NSString *)title lat:(float)latitude lng:(float)longitude address:(NSString *)addres descriptionText:(NSString *)text images:(NSArray *)imgs imageCaptions:(NSArray *)captions audio:(AVURLAsset *)audioAsset;
+
+
+@end
+
---
+++ SaratogaHistory/Models/SHPlace.m
@@ -0,0 +1,38 @@
+//
+//  SHPlace.m
+//  Saratoga History
+//
+//  Created by Kabir Manghnani on 06/21/15.
+//  Copyright (c) 2015 Kabir Manghnani. All rights reserved.
+//
+
+#import "SHPlace.h"
+#import <Parse/Parse.h>
+
+@implementation SHPlace
+
+-(id)initWithIndex:(int)indx title:(NSString *)title lat:(float)latitude lng:(float)longitude address:(NSString *)addres descriptionText:(NSString *)text images:(NSArray *)imgs imageCaptions:(NSArray *)captions audio:(AVURLAsset *)audioAsset {
+    self = [super init];
+    if(self) {
+       
+        self.index = indx;
+        self.placeTitle = title;
+        self.lat = latitude;
+        self.lng = longitude;
+        self.address = addres;
+        self.descriptionText = text;
+        self.images = imgs;
+        self.imageCaptions = captions;
+        self.audioURLAsset = audioAsset;
+        
+        self.annotationThumbnail = [[JPSThumbnail alloc] init];
+        UIImage *image = [UIImage imageWithData:[imgs firstObject]];
+        self.annotationThumbnail.image = image;
+        self.annotationThumbnail.title = self.placeTitle;
+        self.annotationThumbnail.subtitle = [self.imageCaptions firstObject];
+        self.annotationThumbnail.coordinate = CLLocationCoordinate2DMake(self.lat, self.lng);
+    }    
+    return self;
+}
+
+@end
